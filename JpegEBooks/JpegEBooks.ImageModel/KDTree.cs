aa4d0281d9a2ac31ec64dcf2b5239536a2a1c489
Colin <vogon@outlook.com>
2013-11-20 18:28:29 -0800

    rebooted jpeg_ebooks with a different approach toward an image model

---
+++ JpegEBooks/JpegEBooks.ImageModel/KDTree.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace JpegEBooks.ImageModel
+{
+    class KDMap<Coord, V> where Coord : IComparable<Coord>, IEquatable<Coord>
+    {
+        public KDMap(int k)
+        {
+            this.k = k;
+        }
+
+        public void Insert(Coord[] position, V value)
+        {
+            if (position.Length != this.k)
+            {
+                throw new ArgumentException("position has wrong dimensionality");
+            }
+
+            if (root == null)
+            {
+                // 0 nodes in tree; create a node and use it as root
+                Node newRoot = new Node(position, value);
+                root = newRoot;
+            }
+            else
+            {
+                this.root.Insert(0, this.k, position, value);
+            }
+        }
+
+        public V Get(Coord[] position)
+        {
+            if (this.root == null)
+            {
+                return default(V);
+            }
+            else
+            {
+                Node node = this.root.Get(0, this.k, position);
+
+                if (node == null)
+                {
+                    return default(V);
+                }
+                else
+                {
+                    return node.Value;
+                }
+            }
+        }
+
+        private class Node
+        {
+            public Node(Coord[] position, V value)
+            {
+                this.Position = position;
+                this.Value = value;
+            }
+
+            public readonly Coord[] Position;
+
+            public Node Left;
+            public Node Right;
+
+            public readonly V Value;
+
+            public Node Insert(int depth, int k, Coord[] position, V value)
+            {
+                // check to make sure this node isn't already at the new node's position
+                if (StructuralComparisons.StructuralEqualityComparer.Equals(position, this.Position))
+                {
+                    throw new ArgumentException("position is already occupied");
+                }
+
+                // figure out whether position is before or after this node
+                int dimension = depth % k;
+                Coord rootLocation = this.Position[dimension];
+                Coord location = position[dimension];
+
+                // add new node to the subtree rooted here
+                if (location.CompareTo(rootLocation) < 0)
+                {
+                    // child is < this node; add to left subtree
+                    if (this.Left == null)
+                    {
+                        // left subtree is empty; build new node
+                        Node newNode = new Node(position, value);
+
+                        this.Left = newNode;
+                        return newNode;
+                    }
+                    else
+                    {
+                        // recurse
+                        return this.Left.Insert(depth + 1, k, position, value);
+                    }
+                }
+                else
+                {
+                    // child is > this node; add to right subtree
+                    if (this.Right == null)
+                    {
+                        // right subtree is empty; build new node
+                        Node newNode = new Node(position, value);
+
+                        this.Right = newNode;
+                        return newNode;
+                    }
+                    else
+                    {
+                        // recurse
+                        return this.Right.Insert(depth + 1, k, position, value);
+                    }
+                }
+            }
+
+            public Node Get(int depth, int k, Coord[] position)
+            {
+                // check to see if we're already there
+                if (StructuralComparisons.StructuralEqualityComparer.Equals(position, this.Position))
+                {
+                    return this;
+                }
+
+                // figure out whether position is before or after this node
+                int dimension = depth % k;
+                Coord rootLocation = this.Position[dimension];
+                Coord location = position[dimension];
+
+                // search within child subtrees
+                if (location.CompareTo(rootLocation) < 0)
+                {
+                    // child is < this node; search left subtree
+                    if (this.Left == null)
+                    {
+                        // left subtree is empty; search unsuccessful
+                        return null;
+                    }
+                    else
+                    {
+                        // recurse
+                        return this.Left.Get(depth + 1, k, position);
+                    }
+                }
+                else
+                {
+                    // child is > this node; search right subtree
+                    if (this.Right == null)
+                    {
+                        // right subtree is empty; search unsuccessful
+                        return null;
+                    }
+                    else
+                    {
+                        // recurse
+                        return this.Right.Get(depth + 1, k, position);
+                    }
+                }
+            }
+        }
+
+        private readonly int k;
+        private Node root;
+    }
+}
--- JpegEBooks/JpegEBooks.ImageModel/Model.cs
+++ JpegEBooks/JpegEBooks.ImageModel/Model.cs
@@ -1,29 +1,70 @@
 using System;
 using System.Drawing;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace JpegEBooks.ImageModel
 {
-	public class Model
-	{
-		public Model(int maxN, NeighborhoodDelegate neighborhood)
-		{
-            this.Submodels = new MarkovModel[maxN + 1];
-
-            for (int i = 0; i < maxN; i++)
-            {
-                this.Submodels[i] = new MarkovModel(i, neighborhood);
-            }
-		}
-
-        public void LoadImage(Bitmap image)
+    public class Model
+    {
+        public Model(int maxN)
         {
-            foreach (MarkovModel mm in this.Submodels)
+            this.nullaryModel = new SuccessorTileModel();
+            this.naryModels = new KDMap<int, SuccessorTileModel>[maxN];
+        }
+
+        private class SuccessorTileModel
+        {
+            public SuccessorTileModel()
             {
-                mm.LoadFromImage(image);
+                this.counts = new Dictionary<int[,], int>(TwoDIntArrayStructuralEqualityComparer.Instance);
+                this.grandTotal = 0;
+            }
+
+            public void Add(int[,] tile) 
+            {
+                if (this.counts.ContainsKey(tile))
+                {
+                    this.counts[tile] += 1;
+                }
+                else
+                {
+                    this.counts[tile] = 1;
+                }
+            }
+
+            private Dictionary<int[,], int> counts;
+            private int grandTotal;
+        }
+
+        private class TwoDIntArrayStructuralEqualityComparer : IEqualityComparer<int[,]>
+        {
+            static TwoDIntArrayStructuralEqualityComparer()
+            {
+                Instance = new TwoDIntArrayStructuralEqualityComparer();
+            }
+
+            public static TwoDIntArrayStructuralEqualityComparer Instance { get; private set; }
+
+            private TwoDIntArrayStructuralEqualityComparer() { }
+
+            public bool Equals(int[,] x, int[,] y)
+            {
+                return StructuralComparisons.StructuralEqualityComparer.Equals(x, y);
+            }
+
+            public int GetHashCode(int[,] obj)
+            {
+                return StructuralComparisons.StructuralEqualityComparer.GetHashCode(obj);
             }
         }
 
-        private MarkovModel[] Submodels { get; set; }
-	}
+        public void LoadImage(Bitmap image)
+        {
+        }
+
+        private SuccessorTileModel nullaryModel;
+        private KDMap<int, SuccessorTileModel>[] naryModels;
+    }
 }
 
